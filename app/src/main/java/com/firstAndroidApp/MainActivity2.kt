b90481fd04fc29f0326d96f98dc9dabe204bf1b3
paul <kohleangpaul911@gmail.com>
2023-06-04 15:41:26 -0700

    file name change

--- app/src/main/java/com/firstAndroidApp/MainActivity2.kt
+++ app/src/main/java/com/firstAndroidApp/MainActivity2.kt
@@ -2,12 +2,9 @@ package com.firstAndroidApp
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.GridLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.core.content.ContextCompat
-import kotlinx.coroutines.delay
 import java.util.Random
 
 class MainActivity : ComponentActivity() {
