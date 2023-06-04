c5ba2b8c15a3102980608a5bdcb0a8f6bace71cb
paul <kohleangpaul911@gmail.com>
2023-06-04 15:46:06 -0700

    make changes to code base

--- app/src/main/java/com/firstAndroidApp/MainActivity.kt
+++ app/src/main/java/com/firstAndroidApp/MainActivity.kt
@@ -4,23 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.firstAndroidApp.ui.theme.HumanBenchmarkTheme
-import android.widget.GridLayout
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.core.content.ContextCompat
-import kotlinx.coroutines.delay
-import java.util.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -38,11 +21,6 @@ class MainActivity : ComponentActivity() {
         }
     }
     
-}
-
-
-
-    }
 }
 
 
--- app/src/main/java/com/firstAndroidApp/MainActivity2.kt
+++ app/src/main/java/com/firstAndroidApp/MainActivity2.kt
@@ -1,7 +1,7 @@
 package com.firstAndroidApp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 
