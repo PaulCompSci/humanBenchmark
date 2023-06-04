b8eec1167397983d5a5ce05af76746010adc8a17
paul <kohleangpaul911@gmail.com>
2023-06-04 15:53:19 -0700

    make the world great again

--- app/src/main/java/com/firstAndroidApp/MainActivity3.kt
+++ app/src/main/java/com/firstAndroidApp/MainActivity3.kt
@@ -1,7 +1,6 @@
 package com.firstAndroidApp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
