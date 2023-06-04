3c2837aa806732e16679124f941475fe563a96ea
paul <kohleangpaul911@gmail.com>
2023-06-04 10:35:14 -0700

    file cleanup

--- app/src/main/java/com/firstAndroidApp/MainActivity.kt
+++ app/src/main/java/com/firstAndroidApp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.firstAndroidApp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,29 +17,8 @@ import com.firstAndroidApp.ui.theme.HumanBenchmarkTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HumanBenchmarkTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+
+       
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HumanBenchmarkTheme {
-        Greeting("Android")
-    }
-}
