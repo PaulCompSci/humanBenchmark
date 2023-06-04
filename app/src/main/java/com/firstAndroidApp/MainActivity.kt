0b6794a777b579702db0afd5deb1e081c49c0b93
paul <kohleangpaul911@gmail.com>
2023-06-04 15:36:37 -0700

    main functionality done

--- app/src/main/java/com/firstAndroidApp/MainActivity.kt
+++ app/src/main/java/com/firstAndroidApp/MainActivity.kt
@@ -2,23 +2,77 @@ package com.firstAndroidApp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.GridLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.firstAndroidApp.ui.theme.HumanBenchmarkTheme
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.delay
+import java.util.Random
 
 class MainActivity : ComponentActivity() {
+    var level = 0
+    var arr = mutableListOf<Int>()
+    var pointer = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-       
+        setContentView(R.layout.activity2)
+
+         val buttons: Array<Button> = arrayOf(
+             findViewById(R.id.button0),
+            findViewById(R.id.button1),
+            findViewById(R.id.button2),
+             findViewById(R.id.button3),
+             findViewById(R.id.button4),
+             findViewById(R.id.button5),
+             findViewById(R.id.button6),
+             findViewById(R.id.button7),
+             findViewById(R.id.button8)
+        )
+
+        fun rand(start: Int, end: Int): Int {
+            require(!(start > end || end - start + 1 > Int.MAX_VALUE)) { "Illegal Argument" }
+            return Random(System.nanoTime()).nextInt(end - start + 1) + start
+        }
+
+        fun animation() {
+            level++;
+            val text: TextView = findViewById(R.id.level) as TextView
+            text.setText("Level : "+ level)
+            arr.add(rand(0,8))
+            Thread{
+                for (i in arr) {
+                    val button = buttons[i]
+                    Thread.sleep(1000);
+                    button.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+                    button.postDelayed({
+                        button.setBackgroundColor(ContextCompat.getColor(this, R.color.buttonPurple))
+                    }, 500)
+                }
+            }.start()
+        }
+
+        for (i in 0 until 8) {
+            buttons[i].setOnClickListener {
+                buttons[i].setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+                buttons[i].postDelayed({
+                    buttons[i].setBackgroundColor(ContextCompat.getColor(this, R.color.buttonPurple))
+                }, 500)
+                if (i == arr[pointer]) {
+                    pointer++;
+                    if (pointer == arr.size){
+                        pointer = 0;
+                        animation()
+                    }
+                }else{
+
+                }
+            }
+        }
+
+        animation()
+
     }
 }
 
